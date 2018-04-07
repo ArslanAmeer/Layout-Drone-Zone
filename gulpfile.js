afb85860868d7b13e8e5bb97604ac0351e8aa1fd
Arslan Ameer <arslan.ameer@hotmail.com>
2018-04-07 05:08:01 +0500

    Add: Gulp Packages for Dev Envoirnment
    Added gulp with all required Packages for Dev Envoirnment

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+const gulp = require("gulp");
+const less = require("gulp-less");
+const lessAutoPrefix = require("less-plugin-autoprefix");
+const lessGLob = require("less-plugin-glob");
+const cssMin = require("gulp-clean-css");
+const jsMin = require("gulp-uglify-es").default;
+const imageMin = require("gulp-imagemin");
+const htmlMin = require("gulp-htmlmin");
+const sourceMap = require("gulp-sourcemaps");
+const concat = require("gulp-concat");
+const rename = require("gulp-rename");
+const browserSync = require("browser-sync").create();
+
+var lessPrefix = new lessAutoPrefix( { browsers: ["Last 2 Versions"]} );
