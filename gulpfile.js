f1c227a4e84722e0b6adef483de691a909774ebf
Arslan Ameer <arslan.ameer@hotmail.com>
2018-04-19 23:58:41 +0500

    Add: Watch Task (Gulp Task)

--- gulpfile.js
+++ gulpfile.js
@@ -58,3 +58,23 @@ gulp.task("Less", () => {
     .pipe(gulp.dest("dist/assets/css/"))
     .pipe(browserSync.stream());
 })
+
+////  Watch and Serve Task
+
+gulp.task("serve",function(){
+    browserSync.init({
+        server: "dist",
+        port: 2712,
+        host: "192.168.10.66",
+        browser: "Firefox"
+    });
+    gulp.watch("src/assets/less/**/*.less", ["less"]);
+    gulp.watch("src/*.html",["Html-Minify"]);
+    gulp.watch("src/assets/js/*.js",["Js-Minify"]);
+    gulp.watch("src/assets/images/*",["Image-Minify"]);
+    gulp.watch("dist/assets/*html").on("change", browserSync.reload);
+});
+
+// Gulp Default Task 
+
+gulp.task("default",["Image-Minify", "Html-Minify", "Js-Minify", "less", "serve"]);
