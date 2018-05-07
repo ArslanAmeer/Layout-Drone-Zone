8d37b5cc2e35d3a6f80d05924d7225879588acc7
Arslan Ameer <arslan.ameer@hotmail.com>
2018-05-07 18:21:19 +0500

    ADD: OpenStreetMap and Footer
    Google api are not working right now. will Update soon

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -13,3 +13,11 @@ hamBtn.addEventListener("click", () => {
         navMenu.style.display = "none";
     }
 });
+
+function myMap() {
+    var mapProp = {
+        center: new google.maps.LatLng(40.728884, -74.077059),
+        zoom: 5,
+    };
+    var map = new google.maps.Map(document.getElementById("myMap"), mapProp);
+}
